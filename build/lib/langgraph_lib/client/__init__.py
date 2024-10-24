c505bb92514a68b6e23f6332e959e67aad7b11ca
aiwithvd <vishvdeep.dasadiya.vd@gmail.com>
2024-10-24 15:38:50 +0530

    Removing unnecessary builds and library

--- build/lib/langgraph_lib/client/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/langgraph_fastapi/client/__init__.py
-
-from .client import AgentClient
-
-__all__ = ["AgentClient"]
--- build/lib/langgraph_lib/service/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/langgraph_fastapi/service/__init__.py
-
-from .service import create_app
-
-__all__ = ["create_app"]
